08763b02196f362c8c4fc886fac2961aec8f5be1
Joshua Hall <JoshuaHall.CS@gmail.com>
2021-04-01 01:28:05 -0500

    Initial Commit

---
+++ PizzaBox.Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaBox.Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Program.Run();
+        }
+
+        private static void Run()
+        {
+            Console.WriteLine("Welcome to Pizza Box");
+            var stores = new List<string>()
+            {
+                "Store 001",
+                "Store 002",
+                "Store 003"
+            };
+
+            foreach (var item in stores)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+
+    }
+}
